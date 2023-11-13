90f599d1b8058aca3ba2b6ec1f49f98a38e8ee2e
AntonTribu <anton@we-tribu.com>
2023-11-13 22:43:20 +0200

    Code cleanup

--- android/app/src/main/java/com/rickmortyapp/domain/repository/ICharacterRepository.kt
+++ android/app/src/main/java/com/rickmortyapp/domain/repository/ICharacterRepository.kt
@@ -3,7 +3,7 @@ package com.rickmortyapp.domain.repository
 import com.rickmortyapp.domain.model.CharacterPojo
 
 interface ICharacterRepository {
-   suspend fun likedCharacter(id:Int)
-   suspend fun disLikedCharacter(id:Int)
-   suspend fun getCharacter(id:Int):CharacterPojo?
+    suspend fun likedCharacter(id: Int)
+    suspend fun disLikedCharacter(id: Int)
+    suspend fun getCharacter(id: Int): CharacterPojo?
 }
--- android/app/src/main/java/com/rickmortyapp/domain/usecase/GetCharacterUseCase.kt
+++ android/app/src/main/java/com/rickmortyapp/domain/usecase/GetCharacterUseCase.kt
@@ -1,10 +1,8 @@
 package com.rickmortyapp.domain.usecase
 
-import com.rickmortyapp.data.local.CharactersDatabase
-import com.rickmortyapp.data.local.model.CharacterEntity
 import com.rickmortyapp.domain.repository.ICharacterRepository
 import javax.inject.Inject
 
-class GetCharacterUseCase  @Inject constructor(private val repository: ICharacterRepository) {
+class GetCharacterUseCase @Inject constructor(private val repository: ICharacterRepository) {
     suspend fun execute(id: Int) = repository.getCharacter(id)
 }
