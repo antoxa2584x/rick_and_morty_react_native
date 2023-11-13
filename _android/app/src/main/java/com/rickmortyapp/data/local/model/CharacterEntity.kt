75441201e4131fa903fb545eabb053746db07d90
AntonTribu <anton@we-tribu.com>
2023-11-13 20:49:19 +0200

    Remove not worked android dir

--- _android/app/src/main/java/com/rickmortyapp/data/local/model/CharacterEntity.kt
+++
@@ -1,6 +0,0 @@
-package com.rickmortyapp.data.local.model
-
-//
-//@Entity
-//data class CharacterEntity(val isLiked: Boolean, @PrimaryKey val id: Int)
-data class CharacterEntity(val isLiked: Boolean)
